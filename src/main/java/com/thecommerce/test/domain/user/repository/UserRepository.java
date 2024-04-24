e37311f243f622bbfab2c6f358e6d70fa344bf45
김현종 <cowkey206@gmail.com>
2024-04-24 21:46:34 +0900

    Feat : 추가 User JPA Repository

---
+++ src/main/java/com/thecommerce/test/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.thecommerce.test.domain.user.repository;
+
+import com.thecommerce.test.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
